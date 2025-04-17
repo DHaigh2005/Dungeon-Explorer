73826f7aadda3684ad1a108b687ac3e18e14fafd
DHaigh2005 <dylanhaigh0308@gmail.com>
2025-04-17 19:56:25 +0100

    save 2

--- DungeonExplorer/DungeonExplorer/Room.cs
+++ DungeonExplorer/DungeonExplorer/Room.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace DungeonExplorer
 {
-    public class Monster : Creature
-    {
-        public Room CurrentRoom { get; set; }
-        public int 
-    }
+    internal class Room
     {
     }
 }
