1ffa5e5d48db425d06c242ae29a710f93dae0c93
DHaigh2005 <dylanhaigh0308@gmail.com>
2025-04-19 00:59:24 +0100

    added monster class
    added monster class with 6 monsters

--- DungeonExplorer/DungeonExplorer/Monster.cs
+++ DungeonExplorer/DungeonExplorer/Monster.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +14,89 @@ namespace DungeonExplorer
         public static Random random = new Random();
         public Monster(string name, int lowestHealth, int highestHealth, int lowestDamage, int highestDamage) : base(name, random.Next(lowestHealth, highestHealth))
         {
+            GiveDamage = random.Next(lowestDamage, highestDamage);
+        }
+        public bool AttackDelay { get; set; } = true;
+    }
 
+    public class Dragon : Monster
+    {
+        // 50% chance to do the first attack
+        public Dragon() : base("Dragon", 160, 200, 20, 30)
+        {
+            AttackDelay = random.Next(2) == 1;
         }
     }
+
+    public class CaveRat : Monster
     {
+        // never does the first attack
+        public CaveRat() : base("Cave Rat", 10, 20, 1, 3)
+        {
+            AttackDelay = true;
+        }
+    }
+
+    public class Spider : Monster
+    {
+        // 25% chance to do the first attack
+        public Spider() : base("Spider", 10, 15, 15, 25)
+        {
+            AttackDelay = random.Next(4) != 0;
+        }
+    }
+
+    public class Bear : Monster
+    {
+        // 33% chance to do the first attack
+        public Bear() : base("Bear", 120, 160, 12, 18)
+        {
+            AttackDelay = random.Next(3) == 0;
+        }
+    }
+
+    public class Bandit : Monster
+    {
+        // 33% chance to do the first attack
+        public Bandit() : base("Bandit", 60, 80, 8, 14)
+        {
+            AttackDelay = random.Next(3) == 1;
+        }
+    }
+
+    public class Ghost : Monster
+    {
+        // 50% chance to do the first attack
+        public Ghost() : base("Ghost", 40, 60, 6, 12)
+        {
+            AttackDelay = random.Next(2) == 1;
+        }
+    }
+
+    public class GiantBat : Monster
+    {
+        // 33% chance to do the first attack
+        public GiantBat() : base("Giant Bat", 20, 30, 3, 6)
+        {
+            AttackDelay = random.Next(3) != 0;
+        }
+    }
+
+    public class Troll : Monster
+    {
+        // 25% chance to do the first attack
+        public Troll() : base("Troll", 140, 180, 10, 18)
+        {
+            AttackDelay = random.Next(4) != 0;
+        }
+    }
+
+    public class Assassin : Monster
+    {
+        // 80% chance to do the first attack
+        public Assassin() : base("Assassin", 50, 70, 15, 22)
+        {
+            AttackDelay = random.Next(5) != 0;
+        }
     }
 }
--- DungeonExplorer/DungeonExplorer/Room.cs
+++ DungeonExplorer/DungeonExplorer/Room.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace DungeonExplorer
 {
-    internal class Room
+    public class Room
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool ThingHappened { get; set; } = false;
+
+        public List<Item> Item { get; set; }
+        public List<Creature> Creature { get; set; }
+        public Dictionary<string, Room> Exit { get; set; }
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Item = new List<Item>();
+            Creature = new List<Creature>();
+            Exit = new Dictionary<string, Room>();
+        }
+        public void AddExit(Room room)
+        {
+            Exit.Add(room);
+        }
+        public void GiveTheDescription(string, description)
+        {
+            Description = description;
+        }
+        public void MakeNewCreature(Creature creature)
+        {
+            
+        }
+        
+    }
     {
     }
 }
