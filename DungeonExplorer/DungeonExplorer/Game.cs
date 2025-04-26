d7a2d8800ffd240a08e9aea4d9b9618d4c701b0e
DHaigh2005 <dylanhaigh0308@gmail.com>
2025-04-26 16:28:22 +0100

    game class
    made a start on the game class. created a few rooms

--- DungeonExplorer/DungeonExplorer/Game.cs
+++ DungeonExplorer/DungeonExplorer/Game.cs
@@ -11,6 +11,33 @@ namespace DungeonExplorer
 {
     internal class Game
     {
-        private List 
+        private List<Room> Room;
+        private Player player;
+
+        public Game()
+        {
+            player = new Player("Name", 100);
+            Rooms = new List<Room>();
+            CreateRooms();
+
+        }
+
+        List<string> WeaponPhrase = new List<string>()
+        {
+            " is found laying in a chest.", "is conveniently placed on a table in the center of the room.", " looks as if it has been here for centuries."
+        };
+
+        private void CreateRooms()
+        {
+            Room ForgottenGarden = new Room("Forgotten Garden", "You step into a garden that seems to have been forgotten by time. The air smells of decay and death, yet the vines and ivy are strangely still thriving.");
+            Room GrandLibrary = new Room("Grand Library", "The shelves are stacked high with ancient scriptures. The scent of paper, and a burning fireplace fill the air.");
+            Room = new Room("", "");
+            Room = new Room("", "");
+            Room = new Room("", "");
+            Room = new Room("", "");
+            Room = new Room("", "");
+            Room = new Room("", "");
+
+        }
     }
 }
